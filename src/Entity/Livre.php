456e4221a9e09b7d7414ef8612bd16c17e748120
manserman <73804349+manserman@users.noreply.github.com>
2022-04-19 16:52:04 +0200

    Initial commit

---
+++ src/Entity/Livre.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=LivreRepository::class)
+ * @UniqueEntity(fields="isbn", message="cet isbn existe déjà !")
+ */
+class Livre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=150, unique=true)
+     */
+    private $isbn;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbpages;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_parution;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\Regex(pattern="/^[0-9]$/", message=" la valeur doit etre comprise entre 0 et 20")
+     */
+    private $note;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Auteur::class, mappedBy="livres")
+     */
+    private $auteurs;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Genre::class, inversedBy="livres")
+     */
+    private $genres;
+
+    public function __construct()
+    {
+        $this->auteurs = new ArrayCollection();
+        $this->genres = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getNbpages(): ?int
+    {
+        return $this->nbpages;
+    }
+
+    public function setNbpages(int $nbpages): self
+    {
+        $this->nbpages = $nbpages;
+
+        return $this;
+    }
+
+    public function getDateDeParution(): ?\DateTimeInterface
+    {
+        return $this->date_de_parution;
+    }
+
+    public function setDateDeParution(\DateTimeInterface $date_de_parution): self
+    {
+        $this->date_de_parution = $date_de_parution;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Auteur[]
+     */
+    public function getAuteurs(): Collection
+    {
+        return $this->auteurs;
+    }
+
+    public function addAuteur(Auteur $auteur): self
+    {
+        if (!$this->auteurs->contains($auteur)) {
+            $this->auteurs[] = $auteur;
+            $auteur->addLivre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAuteur(Auteur $auteur): self
+    {
+        if ($this->auteurs->removeElement($auteur)) {
+            $auteur->removeLivre($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Genre[]
+     */
+    public function getGenres(): Collection
+    {
+        return $this->genres;
+    }
+
+    public function addGenre(Genre $genre): self
+    {
+        if (!$this->genres->contains($genre)) {
+            $this->genres[] = $genre;
+        }
+
+        return $this;
+    }
+
+    public function removeGenre(Genre $genre): self
+    {
+        $this->genres->removeElement($genre);
+
+        return $this;
+    }
+}
